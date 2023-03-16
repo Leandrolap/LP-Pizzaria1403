dab12e1f1513cb95049796c392764aa2a859d577
Leandro <suporte_desenvolvimento@outlook.com>
2023-03-16 20:06:58 -0300

    Initial commit

---
+++ src/br/newtonpaiva/Hamburguer.java
@@ -0,0 +1,15 @@
+package br.newtonpaiva;
+
+public class Hamburguer {
+    String nome;
+    Double valor;
+    Boolean artesanal;
+
+    public Double calcular(Double calH) {
+        if (artesanal = true)
+            calH += 8.00;
+        return calH;
+    }
+
+
+}
